416294e582b765b6bf07a545e0d471bf381fdda2
James <jamesgault1488@gmail.com>
2021-01-16 11:21:38 -0500

    Created search history for button click/ created local storage for user input/ updated readme to reflect website and websites purpose

--- scripts/script.js
+++ scripts/script.js
@@ -1,18 +1,24 @@
- // + $("#userInput").val() + "&begin_date=20120101&end_date=20121231&api-key=n8ZpZeGBjpYFymrs0Lcvt46sbEJDkNuY"
+// + $("#userInput").val() + "&begin_date=20120101&end_date=20121231&api-key=n8ZpZeGBjpYFymrs0Lcvt46sbEJDkNuY"
+//  ------------------------------ Global Variables -------------------------------
+let searchHistory = [];
 
-
- $("#btn").on("click", () => {
+$("#btn").on("click", () => {
 
     let userInput = $("#userInput").val();
+    var queryURL = "https://watch-here.p.rapidapi.com/wheretowatch?title=" + userInput + "&mediaType=tv%20show";
 
-    var queryURL = "https://netflix-unofficial.p.rapidapi.com/api/search";
+    // ------------------------------------ Setting up local storage -----------------------
 
-    // ----------------------------------------Where to watch API--------------------------------------------------
+    searchHistory.append(userInput.val());
+
+    window.localStorage.setItem('searchHIstory', searchHistory);
+
+    // ---------------------------------------- Where to watch API --------------------------------------------------
 
     const settings = {
         "async": true,
         "crossDomain": true,
-        "url": "https://watch-here.p.rapidapi.com/wheretowatch?title=" + userInput + "&mediaType=tv%20show",
+        "url": queryURL,
         "method": "POST",
         "headers": {
             "content-type": "application/json",
@@ -20,7 +26,7 @@
             "x-rapidapi-host": "watch-here.p.rapidapi.com"
         },
         "processData": false,
-        "data": "{\n    \"mediaType\": \"tv show\",\n    \"title\": \""+ userInput +"\"\n}"
+        "data": "{\n    \"mediaType\": \"tv show\",\n    \"title\": \"" + userInput + "\"\n}"
     };
 
     $.ajax(settings).done(function (response) {
@@ -32,12 +38,12 @@
 
     });
 
-    // ----------------------------------------IMDB API --------------------------------------------------
+    // ---------------------------------------- IMDB API --------------------------------------------------
 
     const settings2 = {
         "async": true,
         "crossDomain": true,
-        "url": "https://movies-tvshows-data-imdb.p.rapidapi.com/?type=get-movies-by-title&title="+ userInput,
+        "url": "https://movies-tvshows-data-imdb.p.rapidapi.com/?type=get-movies-by-title&title=" + userInput,
         "method": "GET",
         "headers": {
             "x-rapidapi-key": "69d811a2bemsh7406c1b512a31f1p10322djsn64aee285beed",
@@ -46,6 +52,13 @@
     };
 
     $.ajax(settings2).done(function (response2) {
+        // ---------------------------------- create search history list ------------------------
+        let ul = document.createElement("ul").textContent = "search history";
+        let li = document.createElement("li");
+        let t = document.createTextNode(userInput.val());
+        li.appendChild(t);
+        ul.appendChild(li);
+
         console.log(response2);
     });
 
