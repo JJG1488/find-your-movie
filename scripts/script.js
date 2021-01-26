737470a9ffa102bed8cc32ed1f1f2d4d25391f03
James <jamesgault1488@gmail.com>
2021-01-25 22:13:28 -0500

    set modal to work on wrong nonsense inputs/ applied switch statement/ no duplicate recent searches

--- scripts/script.js
+++ scripts/script.js
@@ -1,25 +1,31 @@
 // + $("#userInput").val() + "&begin_date=20120101&end_date=20121231&api-key=n8ZpZeGBjpYFymrs0Lcvt46sbEJDkNuY"
 //check for Navigation Timing API support
+// ============================================================  Task  ==================================================================
+// make conditional for text... if text === null... get rid of null from the list
 
-//  ------------------------------ Local Storage -------------------------------===================================
+// ================================================== Local Storage ==================================================================
 // getting user input value
-const inpValue = document.getElementById('userInput')
+const inpValue = document.getElementById('userInput');
 // selecting btn from html
-const btnInsert = document.getElementById('btn')
+const btnInsert = document.getElementById('btn');
 // setting an array for localStorage
-const lsArray = []
+const lsArray = [];
 // selecting a place to display local storage
 const lsOutput = document.getElementById('lsOutput');
 // setting up the last item in the local storage to be selected with getItem() method
-const lastItemInLocalStorage = JSON.parse(window.localStorage.getItem("lsArray"))
+let lastItemInLocalStorage = JSON.parse(window.localStorage.getItem("lsArray"))
+if (lastItemInLocalStorage === null) {
+    lastItemInLocalStorage = [];
+
+}
 // logging last item in local storage to check value
 // console.log(lastItemInLocalStorage[lastItemInLocalStorage.length - 1]);
 
 let searchHistory = JSON.parse(localStorage.getItem("searchHistory"));
-if (searchHistory == undefined || searchHistory == null){
-    searchHistory = ["shrek"];
+if (searchHistory === undefined || searchHistory === null) {
+    searchHistory = [];
 }
-let lastSearchedInput = searchHistory[(searchHistory.length - 1)];
+var lastSearchedInput = searchHistory[(searchHistory.length - 1)];
 
 
 // creating a function for the button click for local storage
@@ -29,15 +35,25 @@ btnInsert.onclick = function () {
     // pushing user input value to the local storage array lsArray
     lsArray.push(value);
     // considtional for if there is a value
+
+    // if(value === null || value === undefined){
+
+    // } else {
+
+    //     getMyMovie(value);
+    // }
+
     if (value) {
         // if there is a value, stringify lsArray and set that value in the local storage
         localStorage.setItem("lsArray", JSON.stringify(lsArray));
         // used to check and make sure that data persists//----> if location.reload() is not commented out,
         // then the previous search history will ONLY show the last result
-        // location.reload()
+        location.reload();
 
     }
-    getMyMovie(value)
+
+    // getMyMovie(value)
+
 }
 
 
@@ -54,21 +70,18 @@ for (let index = 0; index < localStorage.length; index++) {
 }
 
 
+
 //  ------------------------------ Global Variables -------------------------------===================================
-// setting searchHistory array
-// let searchHistory = [];
 // setting foundArray
 let foundArray = [];
 // setting array for major streaming services
 let majorStreamServices = ["Amazon", "Netflix", "Hulu"];
 
 if (performance.type == performance.TYPE_RELOAD) {
-
     getMyMovie(lastItemInLocalStorage[lastItemInLocalStorage.length - 1]);
 }
 
 // creating function to find Netflix
-
 function findNetflix(api, input) {
     let netflixLinkEl = $('#netflix-text');
     for (let index = 0; index < api.length; index++) {
@@ -144,24 +157,67 @@ function previousSearch(searchHistory) {
     }
 }
 
-
 function getMyMovie(input) {
 
     var queryURL = "https://watch-here.p.rapidapi.com/wheretowatch?title=" + input + "&mediaType=tv%20show";
-    
-    // ------------------------------------ Setting up local storage -------------------------------------------------
-    
-    if (searchHistory.includes(input)) {
 
-    }else if (searchHistory.length === 5) {
+    // ======================================= Setting up local storage ============================================================
 
-        searchHistory.shift(0);
-        searchHistory.push(input);
 
-    } else {
+    switch (input) {
+        case null:
+            console.log("null");
+            console.log(searchHistory);
+            break;
+        case undefined:
+            console.log("undefined");
+            console.log(searchHistory);
+            break;
 
-        searchHistory.push(input);
+        default:
+            if (searchHistory.includes(input)) {
+
+            } else {
+
+                console.log("default");
+                searchHistory.push(input);
+            }
+            // console.log(searchHistory);
+            break;
     }
+
+    if (searchHistory.length === 6) {
+        searchHistory.shift(0)
+    }
+    // searchHistory.length === 5
+    // searchHistory.shift(0);
+
+
+    // if (input === null || input === undefined) {
+
+    // console.log("nullified");
+
+
+
+    // (searchHistory.includes(input))
+    // console.log("search history length part ")
+
+    // searchHistory.push(input);
+    // console.log("search history push part");
+    // console.log(searchHistory);
+    // searchHistory.push(input);
+
+
+
+    // else {
+
+    // searchHistory.length === 5
+    // searchHistory.shift(0);
+
+
+    // }
+
+    // searchHistory.push(input)
 
     // console.log(searchHistory)
 
@@ -170,6 +226,7 @@ function getMyMovie(input) {
 
     // function for search on the li element
     $("#search-history").on("click", "li", function () {
+
         // calls getMyMovie() on the text value of the click li element
         getMyMovie($(this).text());
     });
@@ -199,8 +256,8 @@ function getMyMovie(input) {
         findNetflix(parsedResponse, input);
         findHulu(parsedResponse, input);
         findAmazon(parsedResponse, input);
-        console.log("response 1 below");
-        console.log(parsedResponse);
+        // console.log("response 1 below");
+        // console.log(parsedResponse);
         let newArray = [];
         for (let index = 0; index < parsedResponse.length; index++) {
             newArray.push(parsedResponse[index].Watch)
@@ -223,11 +280,26 @@ function getMyMovie(input) {
     };
     $.ajax(settings2).done(function (response2) {
         let imdbID = "";
-        console.log("response 2 below");
+        // console.log("response 2 below");
         console.log(response2);
+        if(response2.search_results === 0){
+            // alert("kjfhsdkhf");
+
+            console.log(searchHistory);
+            searchHistory.pop()
+            localStorage.setItem('searchHistory', JSON.stringify(searchHistory));
+            console.log(searchHistory);
+
+        }
         // console.log(response2.search_results);
         let response2Results = response2.movie_results;
         // console.log(response2Results);
+        if (response2["movie_results"] === undefined) {
+            modalAlert()
+            response2Results = ""
+            // alert("lets get it started")
+        }
+
         for (let i = 0; i < response2Results.length; i++) {
 
             if ((response2Results[i].title).toLowerCase() == input.toLowerCase()) {
@@ -280,11 +352,18 @@ function getMyMovie(input) {
                     }
                 };
                 $.ajax(settings5).done(function (response5) {
-                    // console.log(response5);
-                    let recommendedMovies = response5["movie_results"];
                     $('#recommended').empty();
+                    let recommendedMovies = response5["movie_results"];
+                    if (response5["movie_results"] === undefined) {
+                        modalAlert()
+                        recommendedMovies = ""
+                        // alert("lets get it started")
+                    }
+                    // console.log(typeof response5["movie_results"]);
                     for (let index = 0; index < recommendedMovies.length; index++) {
+
                         $('#recommended').append(`<li class="recommended-list hidden">${recommendedMovies[index].title}</li>`);
+
                     }
 
                     // -------------------- Search History --------------------- //
@@ -292,7 +371,7 @@ function getMyMovie(input) {
 
                     // Displays Document
 
-                    $(".hidden").removeClass("hidden")
+                    $(".hidden").removeClass("hidden");
                     $(".recommended-list").on("click", function () {
                         let recommendedInput = $(this).text();
                         console.log(recommendedInput);
@@ -301,8 +380,14 @@ function getMyMovie(input) {
                     })
                     $(".search-history-list").on("click", function () {
                         let searchHistoryInput = $(this).text();
+                        // if(searchHistoryInput === null || searchHistoryInput === undefined){
 
-                      getMyMovie(searchHistoryInput);
+                        // } else {
+
+                        //     getMyMovie(searchHistoryInput);
+                        // }
+                        getMyMovie(searchHistoryInput)
+
                     })
                 });
             });
@@ -327,20 +412,48 @@ function modalAlert() {
     // Get the <span> element that closes the modal
     var span = document.getElementsByClassName('close')[0];
 
-    modal.classList.add(".show");
-
+    document.querySelector(".thumbnail").classList.add("blur")
     modal.style.display = 'block';
     // When the user clicks on <span> (x), close the modal
 
     span.onclick = function () {
         modal.style.display = 'none';
+        document.getElementById("blur").classList.remove("blur")
     };
     // When the user clicks anywhere outside of the modal, close it
     window.onclick = function (event) {
         if (event.target === modal) {
             modal.style.display = 'none';
+            document.getElementById("blur").classList.remove("blur")
         }
     };
 
 }
+// =============================================================================
+// $(".search-history-list").on("click", function () {
+//     let searchHistoryInput = $(this).text();
+// if(searchHistoryInput === null){
+                // do something
+// }
+//     getMyMovie(searchHistoryInput);
+// })
 
+
+// =============================================================================
+// if search history includes null/ find the index position of null/ remove null
+
+// var searchHistoryListTextChecker = document.getElementById('search-history-list')
+
+// if (searchHistory[0].includes(null, 0)) {
+
+//     let nullSituation = searchHistory.indexOf(null);
+
+//     nullSituation.
+
+//         console.log("this is a null sitation");
+
+
+// }
+
+
+// =============================================================================
